16f69f0b94c56f6d521220b99dc17247e4cc3e18
shivam kushwaha <ahenssolution@gmail.com>
2024-11-20 20:32:57 +0530

    testing the working in windows

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { colors, fontSize } from "../constants/tokens";
 
 const TabsNavigation = () => {
   return (
     // navigation of main app
-    
-    <Tabs>
-        <Tabs.Screen name="favourites"/>
-        <Tabs.Screen name="playlists"/>
-        {/* song navigation folder   */}
-        <Tabs.Screen name="(songs)"/> 
-        <Tabs.Screen name="artists"/>
-    </Tabs>
-  )
-}
 
-export default TabsNavigation 
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: "500",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="favourites" />
+      <Tabs.Screen name="playlists" />
+      {/* song navigation folder   */}
+      <Tabs.Screen name="(songs)" />
+      <Tabs.Screen name="artists" />
+    </Tabs>
+  );
+};
+
+export default TabsNavigation;
